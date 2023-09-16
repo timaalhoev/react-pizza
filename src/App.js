97c21d37bdb3b7654a28441b37946f0ee6120fc6
Timur Alhoev <timuralhoev@yandex.ru>
2023-09-16 17:47:32 +0300

    подключал redux-redux-tolkit

--- src/App.js
+++ src/App.js
@@ -5,17 +5,35 @@ import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
 import NotFoundBlock from "./components/NotFoundBlock";
+
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/slices/filterSlice";
+
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
 
 export const AppContext = React.createContext();
 
-console.log(AppContext);
-
 function App() {
   const [searchValue, setSearchValue] = React.useState("");
+
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
   return (
     <div className="wrapper">
+      <button
+        aria-label="Increment value"
+        onClick={() => dispatch(increment())}
+      >
+        Increment
+      </button>
+      <span>{count}</span>
+      <button
+        aria-label="Decrement value"
+        onClick={() => dispatch(decrement())}
+      >
+        Decrement
+      </button>
       <AppContext.Provider value={{ searchValue, setSearchValue }}>
         <Header />
         <div className="content">
